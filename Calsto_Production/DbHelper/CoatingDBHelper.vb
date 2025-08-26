6d1a506e94280dbbf4f4040e0d11adf2bef3d638
storage_pod11 <storage_pod11@craftsmanautomation.com>
2025-08-26 18:06:35 +0530

    SAUBBUR

---
+++ Calsto_Production/DbHelper/CoatingDBHelper.vb
@@ -0,0 +1,161 @@
+﻿Imports System.Configuration
+Imports System.Data
+Imports Microsoft.Data.SqlClient
+
+Public Class CoatingDBHelper
+
+    Private Shared ReadOnly conString As String = ConfigurationManager.ConnectionStrings("Db_Server").ConnectionString
+
+
+#Region "Project List"
+    Public Shared Function GetProjectNO() As List(Of CoatingProjModel)
+        Dim Projectlist As New List(Of CoatingProjModel)
+        Using con As New SqlConnection(conString)
+            con.Open()
+            Using cmd As New SqlCommand("SELECT * FROM V_PROJ_Coating", con)
+                Using rdr = cmd.ExecuteReader()
+                    While rdr.Read()
+                        Projectlist.Add(New CoatingProjModel With {
+                            .PROJECTNO = rdr("PRO_No").ToString(),
+                            .Customer = rdr("Customer").ToString(),
+                            .Status = rdr("Status").ToString()
+                        })
+                    End While
+                End Using
+            End Using
+        End Using
+        Return Projectlist
+    End Function
+
+#End Region
+
+#Region "Get Coating Jobs"
+    Public Shared Function GetCoatingJobs(proNo As String) As List(Of CoatingModel)
+        Dim CoatingJobs As New List(Of CoatingModel)
+
+        Using con As New SqlConnection(conString)
+            ' Add WHERE clause to use the parameter
+            Dim query As String = "SELECT * FROM V_JOB_Coating WHERE [BOM No.] = @BOMNo"
+            Dim cmd As New SqlCommand(query, con)
+            cmd.Parameters.AddWithValue("@BOMNo", proNo)
+
+            Try
+                con.Open()
+                Dim reader As SqlDataReader = cmd.ExecuteReader()
+                While reader.Read()
+                    Dim job As New CoatingModel With {
+                        .WID = reader("WID").ToString(),
+                        .BOMNo = reader("BOM No.").ToString(),
+                        .BOMName = reader("BOM Name").ToString(),
+                        .JC_no = reader("JC_no").ToString(),
+                        .PartNumber = reader("Part Number").ToString(),
+                        .Description = reader("Description").ToString(),
+                        .Length = If(IsDBNull(reader("Length")), 0D, Convert.ToDecimal(reader("Length"))),
+                        .Width = If(IsDBNull(reader("Width")), 0D, Convert.ToDecimal(reader("Width"))),
+                        .Thick = If(IsDBNull(reader("Thick")), 0D, Convert.ToDecimal(reader("Thick"))),
+                        .UnitWeight = If(IsDBNull(reader("Unit Weight")), 0D, Convert.ToDecimal(reader("Unit Weight"))),
+                        .TotalWeight = If(IsDBNull(reader("Total Weight")), 0D, Convert.ToDecimal(reader("Total Weight"))),
+                        .Colour = reader("Colour").ToString(),
+                        .BOMQty = If(IsDBNull(reader("BOM Qty")), 0, Convert.ToInt32(reader("BOM Qty"))),
+                        .ProducedQty = If(IsDBNull(reader("Produced Qty")), 0, Convert.ToInt32(reader("Produced Qty"))),
+                        .BalanceQty = If(IsDBNull(reader("Balance Qty")), 0, Convert.ToInt32(reader("Balance Qty"))),
+                        .Status = reader("Status").ToString()
+                    }
+                    CoatingJobs.Add(job)
+                End While
+            Catch ex As Exception
+                MessageBox.Show("Error loading Coating jobs: " & ex.Message)
+            End Try
+        End Using
+
+        Return CoatingJobs
+    End Function
+
+#End Region
+
+
+#Region "Get Coating LOT IDs"
+    Public Shared Function GetCoatingLots(Jobno As String) As List(Of CoatingLotModel)
+        Dim Coatinglots As New List(Of CoatingLotModel)
+
+        Using con As New SqlConnection(conString)
+            ' Add WHERE clause to use the parameter
+            Dim query As String = "SELECT * FROM V_JOB_Entry WHERE [JC_no] = @JC_no"
+            Dim cmd As New SqlCommand(query, con)
+            cmd.Parameters.AddWithValue("@JC_no", Jobno)
+
+            Try
+                con.Open()
+                Dim reader As SqlDataReader = cmd.ExecuteReader()
+                While reader.Read()
+                    Dim job As New CoatingLotModel With {
+                        .Transaction_ID = Convert.ToInt32(reader("Transaction_ID")),
+                        .JC_no = reader("JC_no").ToString(),
+                            .Lot_id = reader("Lot_id").ToString(),
+                            .Operation_id = reader("Operation_ID").ToString(),
+                            .Quantity = If(IsDBNull(reader("Quantity")), 0, Convert.ToInt32(reader("Quantity"))),
+                            .Entry_date = If(IsDBNull(reader("Entry_Date")), Nothing, Convert.ToDateTime(reader("Entry_Date"))),
+                            .Done_by = If(IsDBNull(reader("Done_by")), String.Empty, reader("Done_by").ToString()),
+                            .Moved_to = If(IsDBNull(reader("Moved_to")), String.Empty, reader("Moved_to").ToString())
+                    }
+                    Coatinglots.Add(job)
+                End While
+            Catch ex As Exception
+                MessageBox.Show("Error loading Lot IDs: " & ex.Message)
+            End Try
+        End Using
+
+        Return Coatinglots
+    End Function
+
+#End Region
+
+
+    Public Shared Sub CreateJobEntry(JC As String, Qty As Int32, createdBy As String, remark As String)
+        Using con As New SqlConnection(conString)
+            Using cmd As New SqlCommand("sp_CreateLotEntry", con)
+                cmd.CommandType = CommandType.StoredProcedure
+                cmd.Parameters.AddWithValue("@JC_no", JC)
+                cmd.Parameters.AddWithValue("@Quantity", Qty)
+                cmd.Parameters.AddWithValue("@Entered_By", createdBy)
+                cmd.Parameters.AddWithValue("@Remarks", remark)
+                con.Open()
+                cmd.ExecuteNonQuery()
+            End Using
+        End Using
+    End Sub
+
+
+
+
+#Region "Edit Coating LOT"
+    Public Shared Function EditLotEntry(TransId As String, lotId As String, newQty As Integer, updatedBy As String, remarks As String) As Boolean
+        Using con As New SqlConnection(conString)
+            Using cmd As New SqlCommand("sp_UpdateJobCardEntry", con)
+                cmd.CommandType = CommandType.StoredProcedure
+                cmd.Parameters.AddWithValue("@Transaction_ID", TransId)
+                cmd.Parameters.AddWithValue("@JC_no", lotId)
+                cmd.Parameters.AddWithValue("@Quantity", newQty)
+                cmd.Parameters.AddWithValue("@Remarks", remarks)
+                cmd.Parameters.AddWithValue("@UpdatedBy", updatedBy)
+
+                Try
+                    con.Open()
+                    cmd.ExecuteNonQuery()
+                    Return True
+                Catch ex As Exception
+                    MessageBox.Show("Error updating Lot Entry: " & ex.Message)
+                    Return False
+                End Try
+            End Using
+        End Using
+    End Function
+#End Region
+
+
+
+
+
+
+
+End Class
---
+++ Calsto_Production/View/Pages/Coating.xaml.vb
@@ -0,0 +1,344 @@
+﻿Imports System.Collections.ObjectModel
+
+Public Class Coating
+    Inherits Page
+
+    Private CoatingJobList As ObservableCollection(Of CoatingModel)
+    Private LotList As ObservableCollection(Of CoatingLotModel)
+    Private ProjectList As ObservableCollection(Of CoatingProjModel)
+
+    Public Sub New()
+        InitializeComponent()
+        LoadProjects()
+    End Sub
+
+
+    Private Sub LoadProjects()
+        Try
+            Dim data = CoatingDBHelper.GetProjectNO()
+            ProjectList = New ObservableCollection(Of CoatingProjModel)(data)
+            ProjDG.ItemsSource = ProjectList
+        Catch ex As Exception
+            MessageBox.Show("Error loading project list: " & ex.Message)
+        End Try
+    End Sub
+
+    ' Load all Coating jobs
+    Private Sub LoadCoatingJobs(proNo As String)
+        Try
+            Dim data = CoatingDBHelper.GetCoatingJobs(proNo)
+            CoatingJobList = New ObservableCollection(Of CoatingModel)(data)
+            FormDG.ItemsSource = CoatingJobList
+        Catch ex As Exception
+            MessageBox.Show("Error loading project list: " & ex.Message)
+        End Try
+    End Sub
+
+
+    Private Sub LoadLotIDs(jobc As String)
+        Try
+            Dim data = CoatingDBHelper.GetCoatingLots(jobc)
+            LotList = New ObservableCollection(Of CoatingLotModel)(data)
+            JobcardDG.ItemsSource = LotList
+        Catch ex As Exception
+            MessageBox.Show("Error loading project side details: " & ex.Message)
+        End Try
+    End Sub
+
+
+
+
+    Private Sub FormProj_Sel_Changed(sender As Object, e As SelectionChangedEventArgs)
+        Dim selectedProj As CoatingProjModel = TryCast(ProjDG.SelectedItem, CoatingProjModel)
+        If selectedProj IsNot Nothing Then
+            LoadCoatingJobs(selectedProj.PROJECTNO)
+            txt_Projectno.Text = selectedProj.PROJECTNO
+            txt_Customer.Text = selectedProj.Customer
+            Txt_ReqQty.Text = ""
+            Txt_BalQty.Text = ""
+            Txt_Colour.Text = ""
+            Txt_Description.Text = ""
+            Txt_CompQty.Text = ""
+            DetailsTab.IsEnabled = False
+            JobcardDG.ItemsSource = Nothing
+
+        End If
+    End Sub
+
+
+    Private Sub dgLOT_Sel_Changed(sender As Object, e As SelectionChangedEventArgs)
+        Dim selectedlot As CoatingLotModel = TryCast(JobcardDG.SelectedItem, CoatingLotModel)
+        If selectedlot IsNot Nothing Then
+            print_btn.IsEnabled = True
+            editbtn.IsEnabled = True
+            editbtn.Visibility = Visibility.Visible
+            Lotedit.Text = ""
+            Lotedit.Visibility = Visibility.Collapsed
+            Lotapply.Visibility = Visibility.Collapsed
+            editbtn.Visibility = Visibility.Visible
+
+        Else
+            print_btn.IsEnabled = False
+            editbtn.IsEnabled = False
+            editbtn.Visibility = Visibility.Visible
+            editcancel.Visibility = Visibility.Collapsed
+
+        End If
+
+    End Sub
+
+
+    Private Sub Editbtn_Click(sender As Object, e As RoutedEventArgs)
+        Dim selectedlot As CoatingLotModel = TryCast(JobcardDG.SelectedItem, CoatingLotModel)
+
+        If selectedlot IsNot Nothing Then
+            Lotedit.IsEnabled = True
+            Lotedit.Visibility = Visibility.Visible
+            Lotedit.Text = selectedlot.Quantity
+            Lotapply.IsEnabled = True
+            Lotapply.Visibility = Visibility.Visible
+            editbtn.Visibility = Visibility.Collapsed
+            editcancel.IsEnabled = True
+            editcancel.Visibility = Visibility.Visible
+
+        End If
+
+    End Sub
+
+
+
+    Private Sub Cancel_Click(sender As Object, e As RoutedEventArgs)
+        Lotedit.Visibility = Visibility.Collapsed
+        Lotapply.Visibility = Visibility.Collapsed
+        editbtn.Visibility = Visibility.Visible
+
+    End Sub
+
+
+    Private Sub Change_lot_click(sender As Object, e As RoutedEventArgs)
+
+        If JobcardDG.SelectedItem Is Nothing Then
+            MessageBox.Show("Please select a job to Update.")
+            Exit Sub
+        End If
+        Dim selectedlot As CoatingLotModel = TryCast(JobcardDG.SelectedItem, CoatingLotModel)
+
+        If MessageBox.Show($"Update the Qtny for Lot {selectedlot.JC_no}?",
+                       "Confirm Update",
+                       MessageBoxButton.YesNo,
+                       MessageBoxImage.Warning) = MessageBoxResult.No Then
+            Exit Sub
+        End If
+
+        Dim transid As String = selectedlot.Transaction_ID
+        Dim Lotid As String = selectedlot.JC_no
+        Dim newqty As String = Lotedit.Text
+        Dim createdBy As String = Environment.UserName
+        Dim Remarks As String = "Update on:" & Now.ToString("yyyy-MM-dd HH:mm:ss")
+
+        CoatingDBHelper.EditLotEntry(transid, Lotid, newqty, createdBy, Remarks)
+
+        Dim selectedJC As CoatingModel = TryCast(FormDG.SelectedItem, CoatingModel)
+        If selectedJC IsNot Nothing Then
+            LoadLotIDs(selectedJC.JC_no)
+            Txt_ReqQty.Text = selectedJC.BOMQty
+            Txt_BalQty.Text = selectedJC.BalanceQty
+            Txt_Colour.Text = selectedJC.Colour
+            Txt_Description.Text = selectedJC.Description
+            Txt_CompQty.Text = selectedJC.ProducedQty
+
+            If selectedJC.Status = "CLOSED" Then
+                DetailsTab.IsEnabled = False
+            Else
+                DetailsTab.IsEnabled = True
+            End If
+        End If
+
+        Lotedit.Visibility = Visibility.Collapsed
+        Lotapply.Visibility = Visibility.Collapsed
+        editbtn.Visibility = Visibility.Visible
+        Lotedit.Text = ""
+
+        RefreshBOM()
+        Refreshproj()
+
+        Lot_qty.Text = ""
+
+    End Sub
+
+
+
+
+
+
+    Private Sub dgFORM_Sel_Changed(sender As Object, e As SelectionChangedEventArgs)
+        Dim selectedJC As CoatingModel = TryCast(FormDG.SelectedItem, CoatingModel)
+        If selectedJC IsNot Nothing Then
+            LoadLotIDs(selectedJC.JC_no)
+            Txt_ReqQty.Text = selectedJC.BOMQty
+            Txt_BalQty.Text = selectedJC.BalanceQty
+            Txt_Colour.Text = selectedJC.Colour
+            Txt_Description.Text = selectedJC.Description
+            Txt_CompQty.Text = selectedJC.ProducedQty
+
+            If selectedJC.Status = "CLOSED" Then
+                DetailsTab.IsEnabled = False
+            Else
+                DetailsTab.IsEnabled = True
+            End If
+        End If
+
+        Lotedit.Visibility = Visibility.Collapsed
+        Lotapply.Visibility = Visibility.Collapsed
+        editbtn.Visibility = Visibility.Visible
+        Lotedit.Text = ""
+
+
+    End Sub
+
+
+    Private Sub LotEntry_Click(sender As Object, e As RoutedEventArgs)
+        If FormDG.SelectedItem Is Nothing Then
+            MessageBox.Show("Please select a job to apply the lot entry.", "Lot Entry", MessageBoxButton.OK, MessageBoxImage.Warning)
+            Exit Sub
+        End If
+
+        Dim selectedJC As CoatingModel = CType(FormDG.SelectedItem, CoatingModel)
+
+        ' Confirm
+        If MessageBox.Show($"Apply the Qty for this item: {selectedJC.JC_no}?",
+                           "Confirm Lot Entry",
+                           MessageBoxButton.YesNo,
+                           MessageBoxImage.Question) = MessageBoxResult.No Then
+            Exit Sub
+        End If
+
+        ' Validate Qty
+        Dim JCQty As Integer
+        If Not Integer.TryParse(Lot_qty.Text, JCQty) OrElse JCQty <= 0 Then
+            MessageBox.Show("Please enter a valid quantity.", "Invalid Entry", MessageBoxButton.OK, MessageBoxImage.Error)
+            Exit Sub
+        End If
+
+        ' 🔹 Validate against Balance Qty
+        Dim balanceQty As Integer
+        If Not Integer.TryParse(Txt_BalQty.Text, balanceQty) Then
+            balanceQty = 0
+        End If
+
+        If JCQty > balanceQty Then
+            MessageBox.Show($"Lot entry quantity ({JCQty}) cannot be greater than Balance Qty ({balanceQty}).",
+                            "Lot Entry Error",
+                            MessageBoxButton.OK,
+                            MessageBoxImage.Error)
+            Exit Sub
+        End If
+
+        Dim createdBy As String = Environment.UserName
+        Dim Remarks As String = ""
+
+        ' Save to DB
+        CoatingDBHelper.CreateJobEntry(selectedJC.JC_no, JCQty, createdBy, Remarks)
+        RefreshBOM()
+        Refreshproj()
+        Lot_qty.Text = ""
+
+        MessageBox.Show($"Lot entry applied for Job Card {selectedJC} successfully.", "Lot Entry", MessageBoxButton.OK, MessageBoxImage.Information)
+    End Sub
+
+
+    Private Sub RefreshBOM()
+        Try
+            ' 1. Save current grid preset
+            FormDG.SavePreset()
+
+            ' 2. Remember currently selected WID
+            Dim selectedWID As String = Nothing
+            If FormDG.SelectedItem IsNot Nothing Then
+                selectedWID = CType(FormDG.SelectedItem, CoatingModel).WID
+            End If
+
+            ' 3. Get current project number
+            Dim selectedProj = CType(ProjDG.SelectedItem, CoatingProjModel)
+            If selectedProj Is Nothing Then Exit Sub
+            Dim proNo = selectedProj.PROJECTNO
+
+            ' 4. Reload BOM items from database
+            Dim data = CoatingDBHelper.GetCoatingJobs(proNo)
+            CoatingJobList = New ObservableCollection(Of CoatingModel)(data)
+            FormDG.ItemsSource = CoatingJobList
+
+            ' 5. Restore grid preset
+            FormDG.LoadPreset()
+
+            ' 6. Determine which item to select
+            Dim rowToSelect = CoatingJobList.FirstOrDefault(Function(x) x.WID = selectedWID)
+
+            If rowToSelect IsNot Nothing Then
+                ' 7. Select row and scroll into view
+                FormDG.SelectedItem = rowToSelect
+                FormDG.UpdateLayout()
+                FormDG.ScrollIntoView(rowToSelect)
+
+                ' 8. Update detail fields safely
+                Txt_ReqQty.Text = If(rowToSelect.BOMQty.ToString(), "0")
+                Txt_BalQty.Text = If(rowToSelect.BalanceQty.ToString(), "0")
+                Txt_Colour.Text = If(rowToSelect.Colour, String.Empty)
+                Txt_Description.Text = If(rowToSelect.Description, String.Empty)
+                Txt_CompQty.Text = If(rowToSelect.ProducedQty.ToString(), "0")
+
+                ' 9. Enable or disable Details tab based on status
+                DetailsTab.IsEnabled = Not String.Equals(rowToSelect.Status, "CLOSED", StringComparison.OrdinalIgnoreCase)
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show("Error refreshing BOM: " & ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error)
+        End Try
+    End Sub
+
+
+
+
+
+
+    Private Sub Refreshproj()
+        Try
+            ' ==== Refresh Projects ====
+            ProjDG.SavePreset()
+
+            Dim selectedProjNo As String = Nothing
+            If ProjDG.SelectedItem IsNot Nothing Then
+                selectedProjNo = CType(ProjDG.SelectedItem, CoatingProjModel).PROJECTNO
+            End If
+
+            Dim projData = CoatingDBHelper.GetProjectNO()
+            ProjectList = New ObservableCollection(Of CoatingProjModel)(projData)
+            ProjDG.ItemsSource = ProjectList
+
+            ProjDG.LoadPreset()
+
+            ' Restore project selection
+            Dim currentProjNo As String = Nothing
+            If selectedProjNo IsNot Nothing Then
+                Dim projRow = ProjectList.FirstOrDefault(Function(x) x.PROJECTNO = selectedProjNo)
+                If projRow IsNot Nothing Then
+                    ProjDG.SelectedItem = projRow
+                    ProjDG.UpdateLayout()
+                    ProjDG.ScrollIntoView(projRow)
+                    currentProjNo = projRow.PROJECTNO
+
+                End If
+
+
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show("Error refreshing: " & ex.Message)
+        End Try
+    End Sub
+
+
+
+
+
+End Class
